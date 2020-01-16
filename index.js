72d2eb2be18f7ba102308ce16a45b819c92ac530
Dan Johnson <danimal.johnson@gmail.com>
2020-01-16 12:57:55 -0800

    Add environment variables for Heroku

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const server = require('./server.js');
-const port = 8080;
+const dotenv = require('dotenv');
+const buf = Buffer.from('HOSTNAME');
+const opt = { debug: true };
+const config = dotenv.parse(buf, opt);
+// expect a debug message because the buffer is not in KEY=VAL form
+
+const port = process.env.PORT || 8080;
 
 server.listen(port, () => {  
   console.log(`\n *** Server Running on http://localhost:${port} *** `);
